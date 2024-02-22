818d2d67c710809f61d9ac2b562428e159630f55
tkchu <tkchu@users.noreply.github.com>
2024-02-22 10:45:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 import datetime
 
 APPLIST_URL = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
-DETAIL_URL = "http://store.steampowered.com/api/appdetails/?appids={0}"
+DETAIL_URL = "http://store.steampowered.com/api/appdetails/?appids={0}&cc=us&l=en"
 
 def getAppids():
     """获取所有steam游戏列表"""
@@ -35,40 +35,16 @@ def getDetail(appid):
     else:
         return None
 
-def checkNeedUpdateAppid(appid):
-    oldInfo = myinfo.find_one({"appid": appid})
-
-    if not oldInfo:
-        return True
-    if oldInfo and "appid" not in oldInfo:
-        return True
-    if oldInfo and "type" in oldInfo and oldInfo["type"] == "game":
-        if "last_update_time" in oldInfo and (datetime.datetime.now() - oldInfo['last_update_time']).days > 30:
-            return True
-        if "price_overview" in oldInfo and "currency" in oldInfo["price_overview"] and oldInfo["price_overview"]["currency"] != "USD":
-            #return True
-            pass
-    
-    return False
-
-    if oldInfo and "type" in oldInfo and oldInfo["type"] == False:
-    # 如果之前获取失败了，那就不用重新获取了
-        #print str(appid) + ": False"
-        return False
-    elif oldInfo and "type" in oldInfo and oldInfo["type"] != "game":
-    # 如果之前获取过这个不是游戏，那就不用重新获取了
-        #print str(appid) + ": No game"
-        return False
-    elif oldInfo and 'try_times' in oldInfo and oldInfo['try_times']>=5:
-    # 如果尝试的次数很多，那也别试了
-        #print str(appid) + ":already try " + str(oldInfo['try_times']) + " times"
-        return False
-    elif oldInfo and "last_update_time" in oldInfo and (datetime.datetime.now() - oldInfo['last_update_time']).days < 30:
-    # 如果上次获取是在一周以内，那也别获取了
-        #print str(appid) + ":last_update_time" + str(oldInfo['last_update_time']) 
-        return False
-
-    return False
+def getNeedUpdateAppids():
+    result = set()
+    #没有更新过的要更新
+    result.update([info["appid"] for info in myinfo.find({"type":"game","review_update_time":{"$exists":False},"try_times":{"$lt": 5}})])
+    #上次更新到今天超过30天的游戏才会列入更新
+    pass_time = datetime.datetime.now() - datetime.timedelta(days = 30) 
+    result.update([info["appid"] for info in myinfo.find({"type":"game","review_update_time":{"$lt": pass_time},"try_times":{"$lt": 5}})])
+    #临时：不是USD的也要改
+    result.update([info["appid"] for info in myinfo.find({"type":"game","price_overview":{"$exists":True},"price_overview.currency": {"$ne": "USD"}})])
+    return result
 
 def updateDetail(appid, detail):
     newInfo = {"appid":appid,"last_update_time":datetime.datetime.now()}
@@ -76,14 +52,14 @@ def updateDetail(appid, detail):
     if not detail:
         try:
             if not oldInfo:
-                newInfo = merge_two_dicts(newInfo, {"appid":appid,"type":False,"try_times":1})
+                newInfo = merge_two_dicts(newInfo, {"appid":appid,"type":False,"try_times":1}) # try_times已经尝试过的次数
                 myinfo.insert_one(newInfo)
                 print "insert_False_detail:" + str(appid)
             elif oldInfo['type'] == False:
                 myinfo.update_one({"appid":appid}, {'$inc': {'try_times': 1}})
                 print "update_False_detail:" + str(appid)
         except Exception as e:
-            print e
+            print appid,e
     else:
         newInfo = merge_two_dicts(newInfo, detail)
         try:
@@ -94,25 +70,25 @@ def updateDetail(appid, detail):
                 myinfo.insert_one(newInfo)
                 print "insert_New_Info:" + str(appid)
         except Exception as e:
-            print e
+            print appid, e
 
-def mainFindNew():
+def main():
     allappids = set([x for x in getAppids()])
+    #先处理新的游戏
+    print "----INSERTING NEW----"
     allexist = set(myinfo.distinct("appid"))
-    allappids = allappids - allexist
-
-    for i, appid in enumerate(allappids):
-        print "====appid:{0}:{1}/{2}====".format(appid,i,len(allappids))
+    allnew = allappids - allexist
+    for i, appid in enumerate(allnew):
+        print "====new appid:{0}:{1}/{2}====".format(appid,i,len(allnew))
         detail = getDetail(appid)
         updateDetail(appid, detail)
-
-def mainFixAll():
-    allappids = [x for x in getAppids() if checkNeedUpdateAppid(x)]
-
-    for i, appid in enumerate(allappids):
-        print "====appid:{0}:{1}/{2}====".format(appid,i,len(allappids))
+    #然后更新已有的游戏
+    print "----UPDATEING OLD----"
+    allupdate = getNeedUpdateAppids()
+    for i, appid in enumerate(allupdate):
+        print "====update appid:{0}:{1}/{2}====".format(appid,i,len(allupdate))
         detail = getDetail(appid)
         updateDetail(appid, detail)
 
 if __name__ == '__main__':
-    mainFindNew()
+    main()
